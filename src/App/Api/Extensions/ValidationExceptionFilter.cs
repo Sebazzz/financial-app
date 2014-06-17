4a32b7f8f98aa900265b7a0150a9e65808cf3dfe
Sebazzz <triadsebas@gmail.com>
2014-06-17 20:07:24 +0200

    Better error response

--- src/App/Api/Extensions/ValidationExceptionFilter.cs
+++ src/App/Api/Extensions/ValidationExceptionFilter.cs
@@ -1,7 +1,12 @@
 ﻿namespace App.Api.Extensions {
+    using System;
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.Data.Entity.Validation;
+    using System.Linq;
     using System.Net;
+    using System.Net.Http;
+    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
     using System.Web.Http;
@@ -16,8 +21,34 @@
         public override void OnException(HttpActionExecutedContext actionExecutedContext) {
             if (actionExecutedContext.Exception is ValidationException ||
                 actionExecutedContext.Exception is DbEntityValidationException) {
-                throw new HttpResponseException(HttpStatusCode.BadRequest);
+                throw new HttpResponseException(
+                    CreateResponseMessage(actionExecutedContext.Request, actionExecutedContext.Exception));
             }
+        }
+
+        private static HttpResponseMessage CreateResponseMessage(HttpRequestMessage request, Exception ex) {
+            if (ex is ValidationException) {
+                return CreateResponseMessage(request, (ValidationException) ex);
+            }
+
+            return CreateResponseMessage(request, (DbEntityValidationException) ex);
+        }
+
+        private static HttpResponseMessage CreateResponseMessage(HttpRequestMessage request, DbEntityValidationException dbEntityValidationException) {
+            List<object> validationErrors = new List<object>();
+            
+            foreach (DbEntityValidationResult error in dbEntityValidationException.EntityValidationErrors) {
+                validationErrors.Add(new {
+                                             entityType = error.Entry.Entity.GetType(),
+                                             errors = error.ValidationErrors
+                                         });
+            }
+
+            return request.CreateResponse(HttpStatusCode.BadRequest, validationErrors);
+        }
+
+        private static HttpResponseMessage CreateResponseMessage(HttpRequestMessage request, ValidationException validationException) {
+            return request.CreateResponse(HttpStatusCode.BadRequest, validationException.ValidationResult);
         }
     }
 }
