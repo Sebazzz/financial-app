61f6e9c8d8b9ba6b80d6e477755bd3ddbe7ada2c
Sebazzz <triadsebas@gmail.com>
2014-06-24 17:51:46 +0200

    Http request to statistics endpoint

--- src/App/Scripts/App/Controllers/Report.ts
+++ src/App/Scripts/App/Controllers/Report.ts
@@ -2,14 +2,33 @@ module FinancialApp {
     'use strict';
 
     export interface IReportControllerScope extends ng.IScope {
+        isLoaded: boolean;
+
+        stats: DTO.ISheetGlobalStatistics[]
+        incomeChartData: any;
+        expenseChartData: any;
         
     }
 
     export class ReportController {
-        static $inject = ["$scope"];
+        static $inject = ["$scope", "$http"];
 
-        constructor($scope: IReportControllerScope) {
+        constructor(private $scope: IReportControllerScope, private $http: ng.IHttpService) {
+            this.loadStatistics();
+        }
 
+        private loadStatistics() {
+            var stats: ng.IHttpPromise<DTO.ISheetGlobalStatistics[]> = this.$http.get<DTO.ISheetGlobalStatistics[]>("api/sheet/statistics");
+
+            stats.success((data) => {
+                this.$scope.stats = data;
+                this.buildChartData();
+                this.$scope.isLoaded = true;
+            });
+        }
+
+        private buildChartData() {
+          
         }
     } 
 }
