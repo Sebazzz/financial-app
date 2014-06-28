94bf850a4b31b1748a017cb614d6d213ec28012c
Sebazzz <triadsebas@gmail.com>
2014-06-28 13:56:00 +0200

    Do aggresive caching for HTTP views

--- src/App/Support/AngularViewRewriteModule.cs
+++ src/App/Support/AngularViewRewriteModule.cs
@@ -13,11 +13,47 @@
 
         public void Init(HttpApplication context) {
             context.BeginRequest += OnBeginRequest;
+            context.PreSendRequestHeaders += OnPreSendRequestHeaders;
+        }
+
+        private static void OnPreSendRequestHeaders(object sender, EventArgs e) {
+            HttpApplication app = (HttpApplication)sender;
+
+            // if having a 'version' tag in the query string, add caching headers
+            if (!String.IsNullOrEmpty(app.Request.QueryString["v"]))
+            {
+                HttpResponse response = app.Response;
+
+                var contentType = response.ContentType;
+                var contentEncoding = response.ContentEncoding;
+                var status = response.StatusCode;
+
+                response.ClearHeaders();
+
+                response.StatusCode = status;
+                response.ContentEncoding = contentEncoding;
+                response.ContentType = contentType;
+
+                response.Cache.SetCacheability(HttpCacheability.ServerAndPrivate);
+                response.Cache.SetAllowResponseInBrowserHistory(true);
+
+                response.Cache.SetExpires(DateTime.Now.AddYears(1));
+                response.Cache.SetMaxAge(new TimeSpan(365, 0, 0, 0));
+                response.Cache.SetValidUntilExpires(true);
+
+                if (response.StatusCode == 200 || response.StatusCode == 304) {
+                    response.AddFileDependency(app.Request.PhysicalPath);
+
+                    response.Cache.VaryByParams["v"] = true;
+                    response.Cache.SetETagFromFileDependencies();
+                }
+            }
         }
 
         private static void OnBeginRequest(object sender, EventArgs e) {
             HttpApplication app = (HttpApplication) sender;
 
+            // rewrite to mobile if required
             string path = app.Request.Path;
             if (path.EndsWith(".mobile.html", StringComparison.OrdinalIgnoreCase) || !app.Request.Browser.IsMobileDevice) {
                 return;
