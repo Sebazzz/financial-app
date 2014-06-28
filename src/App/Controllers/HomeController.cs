033476773d376356b41f56f1fae1bbb356985555
Sebazzz <triadsebas@gmail.com>
2014-06-28 13:10:52 +0200

    Seperate app version retrieval to seperate service

--- src/App/Controllers/HomeController.cs
+++ src/App/Controllers/HomeController.cs
@@ -3,19 +3,12 @@
     using System.Reflection;
     using System.Web;
     using System.Web.Mvc;
+    using Support;
 
     public sealed class HomeController : Controller {
-        private static readonly string AppVersion = InitVersion();
-        private static string InitVersion() {
-            var versionAttr =
-                typeof (HomeController).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
-            Debug.Assert(versionAttr != null, "This should not be null. Was the assembly properly built?");
-            return versionAttr.InformationalVersion;
-        }
-
         // GET: Home
         public ActionResult Index() {
-            this.ViewBag.AppVersion = AppVersion;
+            this.ViewBag.AppVersion = AppVersion.Informational;
 
             this.Response.Cache.SetAllowResponseInBrowserHistory(true);
             this.Response.Cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
---
+++ src/App/Support/AppVersion.cs
@@ -0,0 +1,19 @@
+﻿namespace App.Support {
+    using System.Diagnostics;
+    using System.Reflection;
+    using Controllers;
+
+    /// <summary>
+    /// Helper class for getting the app version
+    /// </summary>
+    public static class AppVersion {
+        public static readonly string Informational = InitVersion();
+        private static string InitVersion()
+        {
+            var versionAttr =
+                typeof(HomeController).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+            Debug.Assert(versionAttr != null, "This should not be null. Was the assembly properly built?");
+            return versionAttr.InformationalVersion;
+        }
+    }
+}
