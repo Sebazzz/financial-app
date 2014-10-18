b596e863c330c0a450deb49c8d1c35ef054d0acb
Sebazzz <triadsebas@gmail.com>
2014-10-18 11:27:38 +0200

    Remove logging statements

--- src/App/Scripts/App/Directives/RequiredIf.ts
+++ src/App/Scripts/App/Directives/RequiredIf.ts
@@ -16,9 +16,7 @@ module FinancialApp.Directives {
         public link (scope: ng.IScope, instanceElement: ng.IAugmentedJQuery, instanceAttributes: ng.IAttributes, controller: any, transclude: ng.ITranscludeFunction) : void {
             var expr = instanceAttributes["faRequiredIf"]; // fa-required-if
 
-            console.log("fa-required-if expr: %s", expr);
             scope.$watch(expr, (val) => {
-                console.log("fa-required-if: %s", val);
                 if (val) {
                     instanceElement.attr("required", "required");
                 } else {
