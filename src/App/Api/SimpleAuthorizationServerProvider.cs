4aa1984b15de62122504f60537888af8be03a53d
Sebazzz <triadsebas@gmail.com>
2015-02-19 19:31:26 +0100

    Did some refactoring, moved ServerProvider to own file.

---
+++ src/App/Api/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,102 @@
+﻿namespace App.Api {
+    using System;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.Owin.Security;
+    using Microsoft.Owin.Security.OAuth;
+    using Models.Domain.Identity;
+
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider {
+        private static readonly Task Nop = Task.FromResult<Object>(null);
+
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context) {
+            context.Validated();
+            return Nop;
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context) {
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] {"*"});
+
+            using (var repo = ContainerConfig.Container.GetInstance<AppUserManager>()) {
+                var user = await repo.FindAsync(context.UserName, context.Password);
+
+                if (user == null) {
+                    context.SetError("invalid_grant", "The user name or password is incorrect.");
+                    return;
+                }
+
+                await ValidateIdentity(context, user, repo);
+            }
+        }
+
+        private static async Task ValidateIdentity(OAuthGrantResourceOwnerCredentialsContext context, AppUser user, AppUserManager repo) {
+            ClaimsIdentity identity = await user.GenerateUserIdentityAsync(repo, context.Options.AuthenticationType);
+            context.Validated(identity);
+        }
+
+        /// <summary>
+        /// Called when a request to the Token endpoint arrives with a "grant_type" of any other value. If the application supports custom grant types
+        ///             it is entirely responsible for determining if the request should result in an access_token. If context.Validated is called with ticket
+        ///             information the response body is produced in the same way as the other standard grant types. If additional response parameters must be
+        ///             included they may be added in the final TokenEndpoint call.
+        ///             See also http://tools.ietf.org/html/rfc6749#section-4.5
+        /// </summary>
+        /// <param name="context">The context of the event carries information in and results out.</param>
+        /// <returns>
+        /// Task to enable asynchronous execution
+        /// </returns>
+        public override Task GrantCustomExtension(OAuthGrantCustomExtensionContext context) {
+            if (context.GrantType == "impersonate") {
+                return GrantImpersonation(context);
+            }
+
+            return Nop;
+        }
+
+        private static async Task GrantImpersonation(OAuthGrantCustomExtensionContext context) {
+            string ticketString = context.Parameters["ticket"];
+            AuthenticationTicket ticket = context.Options.AccessTokenFormat.Unprotect(ticketString);
+
+            if (ticket == null) {
+                context.SetError("invalid_grant", "The access token is invalid.");
+                return;
+            }
+
+            using (var repo = ContainerConfig.Container.GetInstance<AppUserManager>()) {
+                string userIdString = ticket.Identity.GetUserId();
+                var currentUser = await repo.FindByIdAsync(Int32.Parse(userIdString));
+
+                if (currentUser == null) {
+                    context.SetError("invalid_grant", "The access token is invalid.");
+                    return;
+                }
+
+                AppUser targetUser = await GetTargetUser(context, repo);
+
+                if (targetUser == null || !targetUser.TrustedUsers.Contains(currentUser)) {
+                    context.SetError("invalid_parameter", "The impersonation user id is invalid.");
+                    return;
+                }
+
+                await ValidateIdentity(context, targetUser, repo);
+            }
+        }
+
+        private static async Task<AppUser> GetTargetUser(OAuthGrantCustomExtensionContext context, AppUserManager repo) {
+            string targetUserIdString = context.Parameters["userid"];
+            int targetUserId;
+            Int32.TryParse(targetUserIdString, out targetUserId);
+
+            var targetUser = await repo.FindByIdAsync(targetUserId);
+            return targetUser;
+        }
+
+        private static async Task ValidateIdentity(OAuthGrantCustomExtensionContext context, AppUser user, AppUserManager repo) {
+            ClaimsIdentity identity = await user.GenerateUserIdentityAsync(repo, context.Options.AuthenticationType);
+            context.Validated(identity);
+        }
+    }
+
+}
--- src/App/App_Start/OwinStartup.cs
+++ src/App/App_Start/OwinStartup.cs
@@ -1,32 +1,25 @@
-﻿using Microsoft.Owin;
+﻿using App;
+using Microsoft.Owin;
 
-[assembly: OwinStartup(typeof(App.OwinStartup))]
+[assembly: OwinStartup(typeof (OwinStartup))]
 
 namespace App {
     using System;
     using System.Data.Entity;
-    using System.Diagnostics;
-    using System.Runtime.InteropServices;
-    using System.Security.Claims;
-    using System.Threading.Tasks;
     using System.Web.Http;
-    using Microsoft.AspNet.Identity;
-    using Microsoft.AspNet.Identity.Owin;
-    using Microsoft.Owin.Security;
-    using Microsoft.Owin.Security.Cookies;
-    using Microsoft.Owin.Security.Infrastructure;
+    using Api;
+    using Microsoft.Owin.Cors;
     using Microsoft.Owin.Security.OAuth;
     using Migrations;
     using Models.Domain;
     using Models.Domain.Identity;
     using Owin;
-    using SimpleInjector;
 
     public sealed class OwinStartup {
         public void Configuration(IAppBuilder app) {
             ContainerConfig.Configure();
 
-            HttpConfiguration config = new HttpConfiguration();
+            var config = new HttpConfiguration();
             ContainerConfig.SetUpIntegration(config);
 
             this.ConfigureAuth(app);
@@ -37,21 +30,23 @@ namespace App {
             MvcConfig.Register();
             BundleConfig.Register();
 
-            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseCors(CorsOptions.AllowAll);
             app.UseWebApi(config);
 
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<AppDbContext, Configuration>());
         }
 
         private void ConfigureOAuth(IAppBuilder app) {
-            OAuthAuthorizationServerOptions authServerOptions = new OAuthAuthorizationServerOptions()
-            {
-                AllowInsecureHttp = true,
-                TokenEndpointPath = new PathString("/api/token"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(365),
-                Provider = new SimpleAuthorizationServerProvider()
-            };
- 
+            var authServerOptions = new OAuthAuthorizationServerOptions {
+                                                                            AllowInsecureHttp = true,
+                                                                            TokenEndpointPath =
+                                                                                new PathString("/api/token"),
+                                                                            AccessTokenExpireTimeSpan =
+                                                                                TimeSpan.FromDays(365),
+                                                                            Provider =
+                                                                                new SimpleAuthorizationServerProvider()
+                                                                        };
+
             // Token Generation
             app.UseOAuthAuthorizationServer(authServerOptions);
             app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
@@ -77,90 +72,4 @@ namespace App {
             });*/
         }
     }
-
-    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider {
-        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context) {
-            context.Validated();
-        }
-
-        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context) {
-
-            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] {"*"});
-
-            using (var repo = ContainerConfig.Container.GetInstance<AppUserManager>()) {
-                var user = await repo.FindAsync(context.UserName, context.Password);
-
-                if (user == null) {
-                    context.SetError("invalid_grant", "The user name or password is incorrect.");
-                    return;
-                }
-
-                await ValidateIdentity(context, user, repo);
-            }
-        }
-
-        private static async Task ValidateIdentity(OAuthGrantResourceOwnerCredentialsContext context, AppUser user, AppUserManager repo) {
-            ClaimsIdentity identity = await user.GenerateUserIdentityAsync(repo, context.Options.AuthenticationType);
-            context.Validated(identity);
-        }
-
-        /// <summary>
-        /// Called when a request to the Token endpoint arrives with a "grant_type" of any other value. If the application supports custom grant types
-        ///             it is entirely responsible for determining if the request should result in an access_token. If context.Validated is called with ticket
-        ///             information the response body is produced in the same way as the other standard grant types. If additional response parameters must be
-        ///             included they may be added in the final TokenEndpoint call.
-        ///             See also http://tools.ietf.org/html/rfc6749#section-4.5
-        /// </summary>
-        /// <param name="context">The context of the event carries information in and results out.</param>
-        /// <returns>
-        /// Task to enable asynchronous execution
-        /// </returns>
-        public override Task GrantCustomExtension(OAuthGrantCustomExtensionContext context) {
-            if (context.GrantType == "impersonate") {
-                return GrantImpersonation(context);
-            }
-
-            return Task.FromResult<Object>(null);
-        }
-
-        private static async Task GrantImpersonation(OAuthGrantCustomExtensionContext context) {
-            string ticketString = context.Parameters["ticket"];
-            AuthenticationTicket ticket = context.Options.AccessTokenFormat.Unprotect(ticketString);
-
-            if (ticket == null) {
-                context.SetError("invalid_grant", "The access token is invalid.");
-                return;
-            }
-
-            using (var repo = ContainerConfig.Container.GetInstance<AppUserManager>()) {
-                string userIdString = ticket.Identity.GetUserId();
-                var currentUser = await repo.FindByIdAsync(Int32.Parse(userIdString));
-
-                if (currentUser == null) {
-                    context.SetError("invalid_grant", "The access token is invalid.");
-                    return;
-                }
-
-                string targetUserIdString = context.Parameters["userid"];
-                int targetUserId;
-                Int32.TryParse(targetUserIdString, out targetUserId);
-
-                var targetUser = await repo.FindByIdAsync(targetUserId);
-
-                if (targetUser == null || !targetUser.TrustedUsers.Contains(currentUser)) {
-                    context.SetError("invalid_parameter", "The impersonation user id is invalid.");
-                    return;
-                }
-
-                await ValidateIdentity(context, targetUser, repo);
-            }
-        }
-
-        private static async Task ValidateIdentity(OAuthGrantCustomExtensionContext context, AppUser user, AppUserManager repo) {
-            ClaimsIdentity identity = await user.GenerateUserIdentityAsync(repo, context.Options.AuthenticationType);
-            context.Validated(identity);
-        }
-    }
-
-
 }
