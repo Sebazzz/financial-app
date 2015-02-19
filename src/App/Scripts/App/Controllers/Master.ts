93d5cb112d1dd70e7b3545db1f02dbe79b57ebce
Sebazzz <triadsebas@gmail.com>
2015-02-19 19:59:20 +0100

    Detect being logged out; redirect when so.

--- src/App/Scripts/App/Controllers/Master.ts
+++ src/App/Scripts/App/Controllers/Master.ts
@@ -11,21 +11,34 @@ module FinancialApp {
     }
 
     export class MasterController {
-        static $inject = ["$scope", "authentication"]
+        static $inject = ["$scope", "$location", "authentication"];
 
-        private authentication: Services.AuthenticationService;
-
-        constructor($scope: IMasterControllerScope, authentication: Services.AuthenticationService) {
-            this.authentication = authentication;
+        constructor($scope: IMasterControllerScope, private $location : ng.ILocationService, private authentication: Services.AuthenticationService) {
+            $scope.$on("$locationChangeSuccess", (ev) => this.checkLocationAuthorization())
 
             MasterController.setAuthenticationInfo($scope, this.authentication.isAuthenticated());
 
             this.authentication.addAuthenticationChange(() => MasterController.setAuthenticationInfo($scope, this.authentication.isAuthenticated()));
+            this.checkLocationAuthorization();
         }
 
         private static setAuthenticationInfo($scope: IMasterControllerScope, isAuthenticated : boolean): void {
             $scope.isMenuVisible = isAuthenticated;
             $scope.isAuthenticated = isAuthenticated;
+        }
+
+        private checkLocationAuthorization() {
+            var isLoginPage = this.$location.path().indexOf("/auth/login") !== -1;
+
+            if (!isLoginPage && !this.authentication.isAuthenticated()) {
+                console.warn("Not logged in for path %s, redirecting...", this.$location.path());
+                
+                this.$location.search({
+                    uri: this.$location.path()
+                });
+                this.$location.path("/auth/login");
+                this.$location.replace();
+            }
         }
     }    
 }
