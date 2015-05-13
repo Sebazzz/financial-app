2ef82fb3393967674a210e791eae1d51d33ad53c
Sebazzz <triadsebas@gmail.com>
2015-05-13 09:48:52 +0200

    More force

--- src/App/Scripts/App/AppInit.ts
+++ src/App/Scripts/App/AppInit.ts
@@ -64,6 +64,18 @@ module FinancialApp {
                 $httpProvider.interceptors.push('authenticationTokenInterceptor');
                 $httpProvider.interceptors.push('authenticationCheckInterceptor');
                 $httpProvider.interceptors.push('viewFingerPrintInterceptor');
+
+                //initialize get if not there
+                if (!$httpProvider.defaults.headers.get) {
+                    $httpProvider.defaults.headers.get = {};
+                }    
+
+                //disable IE ajax request caching
+                $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+
+                // extra
+                $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+                $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
             }).withInject("$routeProvider", "$locationProvider", "$httpProvider"));
 
             // constants
