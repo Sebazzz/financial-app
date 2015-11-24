6d875a58ff387063151383475f463dcfb6b93e96
Sebazzz <triadsebas@gmail.com>
2015-11-24 22:01:58 +0100

    Fix app cache mapping

--- src/App/Support/Integration/ApplicationCacheExtensions.cs
+++ src/App/Support/Integration/ApplicationCacheExtensions.cs
@@ -3,7 +3,7 @@ namespace App.Support.Integration {
 
     public static class ApplicationCacheExtensions {
         public static void MapApplicationCacheManifest(this IApplicationBuilder app) {
-            app.Map("application.appcache", a => a.UseApplicationCacheManifest());
+            app.Map("/application.appcache", a => a.UseApplicationCacheManifest());
         }
 
         public static IApplicationBuilder UseApplicationCacheManifest(this IApplicationBuilder app) {
