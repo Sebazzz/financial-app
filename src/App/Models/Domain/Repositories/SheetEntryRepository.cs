1a067ba92c9526202761a8a201af7ce693fbc76f
Sebazzz <triadsebas@gmail.com>
2015-11-27 00:03:35 +0100

    Restore the sort order function

--- src/App/Models/Domain/Repositories/SheetEntryRepository.cs
+++ src/App/Models/Domain/Repositories/SheetEntryRepository.cs
@@ -1,16 +1,15 @@
 ﻿namespace App.Models.Domain.Repositories {
     using System;
     using System.Data.SqlClient;
+    using Microsoft.Data.Entity;
 
     partial class SheetEntryRepository {
         public void ReplaceSortOrder(Sheet sheet, int oldSortOrder, int newSortOrder) {
-            throw new NotImplementedException();
-
-            //this._dbContext.Database.ExecuteSqlCommand(
-            //    "UPDATE SheetEntry SET SortOrder = @newSortOrder WHERE SortOrder = @oldSortOrder WHERE Sheet_Id = @sheetId",
-            //    new SqlParameter("newSortOrder", newSortOrder), 
-            //    new SqlParameter("oldSortOrder", oldSortOrder),
-            //    new SqlParameter("sheetId", sheet.Id));
+            this._dbContext.Database.ExecuteSqlCommand(
+                "UPDATE SheetEntry SET SortOrder = @newSortOrder WHERE SortOrder = @oldSortOrder WHERE Sheet_Id = @sheetId",
+                new SqlParameter("newSortOrder", newSortOrder),
+                new SqlParameter("oldSortOrder", oldSortOrder),
+                new SqlParameter("sheetId", sheet.Id));
         }
     }
 }
