73f0998ca56c2b40ba578a27df977c66f069119a
Sebazzz <triadsebas@gmail.com>
2015-11-27 00:09:59 +0100

    EF7 uses 'hard coded' p0 till p'n' parameters in the ExecuteSqlCommand query.

--- src/App/Models/Domain/Repositories/SheetEntryRepository.cs
+++ src/App/Models/Domain/Repositories/SheetEntryRepository.cs
@@ -1,15 +1,13 @@
 ﻿namespace App.Models.Domain.Repositories {
-    using System;
-    using System.Data.SqlClient;
     using Microsoft.Data.Entity;
 
     partial class SheetEntryRepository {
         public void ReplaceSortOrder(Sheet sheet, int oldSortOrder, int newSortOrder) {
             this._dbContext.Database.ExecuteSqlCommand(
-                "UPDATE SheetEntry SET SortOrder = @newSortOrder WHERE SortOrder = @oldSortOrder WHERE Sheet_Id = @sheetId",
-                new SqlParameter("newSortOrder", newSortOrder),
-                new SqlParameter("oldSortOrder", oldSortOrder),
-                new SqlParameter("sheetId", sheet.Id));
+                "UPDATE dbo.SheetEntry SET SortOrder = @p0 WHERE SortOrder = @p1 AND SheetId = @p2",
+                newSortOrder,
+                oldSortOrder,
+                sheet.Id);
         }
     }
 }
