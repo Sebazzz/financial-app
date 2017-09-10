930fec697f75b4b2d49d5aaa669501372f380871
Sebazzz <triadsebas@gmail.com>
2017-09-10 11:21:11 +0200

    Add manage default page and use codegen for page factory

--- src/App/wwwroot/js/PageFactory.ts
+++ src/App/wwwroot/js/PageFactory.ts
@@ -1,12 +1,15 @@
-﻿import DefaultPage from './Pages/DefaultPage';
-import AuthLoginPage from './Pages/Auth/LoginPage';
-import AuthLogOffPage from './Pages/Auth/LogOffPage';
-import AppContext from './AppFramework/AppContext';
+﻿import AppContext from './AppFramework/AppContext';
+import DefaultPage from './Pages/DefaultPage';
+import Auth_LoginPage from './Pages/Auth/LoginPage';
+import Auth_LogOffPage from './Pages/Auth/LogOffPage';
+import Manage_DefaultPage from './Pages/Manage/DefaultPage';
 
 export default function getPages(appContext : AppContext) {
     return [
         new DefaultPage(appContext),
-        new AuthLoginPage(appContext),
-        new AuthLogOffPage(appContext)
+        new Auth_LoginPage(appContext),
+        new Auth_LogOffPage(appContext),
+        new Manage_DefaultPage(appContext)
     ];
 }
+
---
+++ src/App/wwwroot/js/Pages/Manage/DefaultPage.ts
@@ -0,0 +1,17 @@
+﻿import {Page} from '../../AppFramework/Page'
+import AppContext from '../../AppFramework/AppContext'
+
+export default class DefaultPage extends Page {
+    constructor(appContext: AppContext) {
+        super(appContext);
+
+        this.templateName = 'Default';
+        this.routes = [
+            { name: 'manage', path: '/', forwardTo: 'default' }
+        ];
+    }
+
+    protected onActivate(args?: any): Promise<void> {
+        return Promise.resolve();
+    }
+} 
