49bc55279ff4d944448989127cfa62cd762000c2
Sebazzz <triadsebas@gmail.com>
2017-09-14 00:13:54 +0200

    Implement adding/editing recurring sheet entries
    Use cleave.js for number entering and implement binding handler for that. Change API controller so it does not use the WebApi compatibility shim anymore.
    
    Outstanding issues:
    - Can't get proper cleave.js typings to work (needs more investigation)
    - Bootstrap bug: input-group validation prevent validation message to show (fixable, known bug)
    - Validation for delta triggers only after validation for other properties has completed

---
+++ src/App/wwwroot/js/BindingHandlers/Cleave.ts
@@ -0,0 +1,46 @@
+﻿import * as Cleave from 'cleave.js';
+import * as ko from 'knockout';
+import * as $ from 'jquery';
+
+ko.bindingHandlers['cleaveNumber'] = ko.bindingHandlers['cleaveCurrency'] = {
+    init(element: HTMLInputElement, valueAccessor: () => any, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel?: any, bindingContext?: KnockoutBindingContext): void {
+        const isCurrency = !!allBindingsAccessor.get('cleaveCurrency');                                                                                                                                  
+        const scale = allBindingsAccessor.get('cleaveNumberScale') || 2;
+        const prefix = isCurrency ? '€' : undefined;
+        const cleave = new Cleave(element, { numeral: true, numeralDecimalScale: scale, prefix: prefix });
+
+        ko.computed(() => {
+            let rawValue = ko.unwrap(valueAccessor());
+
+            if (rawValue === null || typeof rawValue === 'undefined') {
+                rawValue = '';
+            }
+
+            // Trigger JQuery event if necesary
+            $(element).trigger('change');
+
+            cleave.setRawValue(rawValue);
+        }, this, { disposeWhenNodeIsRemoved: element });
+
+        function updateObservable() {
+            const obs = valueAccessor();
+            if (ko.isWriteableObservable(obs)) {
+                let rawValue = cleave.getRawValue();
+
+                if (rawValue === null || typeof rawValue === 'undefined' || rawValue === '') {
+                    rawValue = null;
+                } else {
+                    rawValue = rawValue.substr(prefix ? prefix.length : 0);
+                    rawValue = parseFloat(rawValue);
+                    console.assert(!isNaN(rawValue), 'Error parsing value');
+                }
+
+                obs(rawValue);
+            }
+        }
+
+        element.addEventListener('blur', updateObservable);
+        ko.utils.domNodeDisposal.addDisposeCallback(element, () => cleave.destroy());
+        ko.utils.domNodeDisposal.addDisposeCallback(element, () => element.removeEventListener('blur', updateObservable));
+    }
+}
