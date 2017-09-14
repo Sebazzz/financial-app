2a14637265866147c332529ce8ae7652dc4d7f10
Sebazzz <triadsebas@gmail.com>
2017-09-14 18:45:27 +0200

    Ensure routes behind specific paths fail by 404 instead of returning the app bootstrapping HTML

---
+++ src/App/Support/Integration/NotFoundExtensions.cs
@@ -0,0 +1,17 @@
+﻿// ******************************************************************************
+//  © 2017 Ernst & Young - www.ey.com | www.beco.nl
+// 
+//  Author          : Ernst & Young - Cleantech and Sustainability
+//  File:           : NotFoundExtensions.cs
+//  Project         : App
+// ******************************************************************************
+
+namespace App.Support.Integration {
+    using Microsoft.AspNetCore.Routing;
+
+    public static class NotFoundExtensions {
+        public static void MapFailedRoute(this IRouteBuilder routeBuilder, string template) {
+            routeBuilder.MapRoute(template, NotFoundHandler.Execute);
+        }
+    }
+}
---
+++ src/App/Support/Integration/NotFoundHandler.cs
@@ -0,0 +1,26 @@
+﻿// ******************************************************************************
+//  © 2017 Ernst & Young - www.ey.com | www.beco.nl
+// 
+//  Author          : Ernst & Young - Cleantech and Sustainability
+//  File:           : NotFoundHandler.cs
+//  Project         : App
+// ******************************************************************************
+
+namespace App.Support.Integration {
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Http;
+
+    public static class NotFoundHandler {
+        public static Task Execute(HttpContext ctx) {
+            HttpResponse response = ctx.Response;
+
+            response.ContentType = "text/plain";
+            response.StatusCode = 404;
+
+            string errorMessage = $"Requested path {ctx.Request.Path} is not found on the server.";
+
+            return response.WriteAsync(errorMessage, ctx.RequestAborted);
+        }
+    }
+}
