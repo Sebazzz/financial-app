dbb9a20e7ba4af62c2382b3941db98fb115a5859
Sebazzz <triadsebas@gmail.com>
2017-09-24 20:35:38 +0200

    Ubuntu publishing improvements, launch script

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -1,17 +1,46 @@
 ﻿namespace App {
-    using System.IO;
+    using System;
+    using System.Net;
+    using System.Security.AccessControl;
+
+    using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Server.Kestrel.Core;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
 
     public class Program {
         public static void Main(string[] args) {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+            IWebHost host = 
+                WebHost.CreateDefaultBuilder(args)
+                       .ConfigureServices(ConfigureServerOptions)
+                       .ConfigureAppConfiguration(cfg => cfg.AddApplicationInsightsSettings())
+                       .UseKestrel()
+                       .UseStartup<Startup>()
+                       .Build();
 
             host.Run();
+        }
+
+        private static void ConfigureServerOptions(WebHostBuilderContext wc, IServiceCollection sc) {
+            sc.Configure<KestrelServerOptions>(options => {
+                options.AddServerHeader = false;
+                options.UseSystemd();
+                
+                HttpsServerOptions httpsOptions = wc.Configuration.GetSection("server").GetSection("https").Get<HttpsServerOptions>();
+
+                if (httpsOptions?.CertificatePath != null) {
+                    options.Listen(IPAddress.Any, 80);
+                    options.Listen(IPAddress.Any, 443, opts => {
+                        opts.UseHttps(httpsOptions.CertificatePath, httpsOptions.CertificatePassword);
+                    });
+                }
+            });
+        }
+
+        public sealed class HttpsServerOptions {
+            public string CertificatePath { get; set; }
+            public string CertificatePassword { get; set; }
         }
     }
 }
