1abb2c7225a4981b9505f4cbf184f9f5b2fb96b1
Sebazzz <triadsebas@gmail.com>
2017-09-24 12:53:05 +0200

    Rename webpack files so the group together nicely.

--- src/App/webpack.config.js
+++ src/App/webpack.config.js
@@ -2,10 +2,10 @@
 const merge = require('webpack-merge');
 
 const isProduction = process.env.NODE_ENV === 'production',
-      envFile = isProduction ? './webpack.prod.js' : './webpack.dev.js';
+      envFile = isProduction ? './webpack.config.prod.js' : './webpack.config.dev.js';
 
 // Include the correct file dependending if we're building for production or not
-const commonConfig = require('./webpack.common.js'),
+const commonConfig = require('./webpack.config.common.js'),
       envConfig = require(envFile);
 
 module.exports = merge(commonConfig, envConfig);
