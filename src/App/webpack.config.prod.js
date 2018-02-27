a1063be44f729b234bed1933cd4b6240519bea1b
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2018-02-27 21:55:59 +0100

    Got the production build compiling

--- src/App/webpack.config.prod.js
+++ src/App/webpack.config.prod.js
@@ -3,8 +3,6 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
-const webpack = require("webpack");
-
 // Extract compiled CSS into a seperate file
 const path = require('path');
 const targetDir = path.resolve(__dirname, 'wwwroot/build');
@@ -14,39 +12,34 @@ const extractSass = new ExtractTextPlugin({
 
 module.exports = {
     stats: "detailed",
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                parallel: true,
+                uglifyOptions: {
+                    compress: {
+                        dead_code: true,
+                        drop_console: true,
+                        drop_debugger: true,
+                        global_defs: {
+                            DEBUG: false,
+                            "module.hot": false,
+                        },
+                        passes: 2,
+                        warnings: true,
+                    },
+                    output: {
+                        beautify: false,
+                    },
+                    ecma: 5,
+                },
+                warningsFilter: () => true,
+            })
+        ]
+    },
     plugins: [
         new CleanWebpackPlugin([targetDir]),
-        extractSass,
-
-        // Ensure modules name change when the contents change (cache busting)
-        new webpack.HashedModuleIdsPlugin({
-            hashFunction: "sha256",
-            hashDigest: "hex",
-            hashDigestLength: 20,
-        }),
-
-        // Minification
-        new UglifyJsPlugin({
-            parallel: true,
-            uglifyOptions: {
-                compress: {
-                    dead_code: true,
-                    drop_console: true,
-                    drop_debugger: true,
-                    global_defs: {
-                        DEBUG: false,
-                        "module.hot": false,
-                    },
-                    passes: 2,
-                    warnings: true,
-                },
-                output: {
-                    beautify: false,
-                },
-                ecma: 5,
-            },
-            warningsFilter: () => true,
-        }),
+        extractSass
     ],
     module: {
         rules: [
