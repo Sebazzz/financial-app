d240c81a8f3c352eb610f71824d0023b0ba1cd36
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2018-04-02 22:14:10 +0200

    Allow enabling two-factor authentication. Logging in is not working yet though.

--- src/App/Api/AccountController.cs
+++ src/App/Api/AccountController.cs
@@ -5,12 +5,19 @@
 //  Project         : App
 // ******************************************************************************
 
+using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
 using System.Threading.Tasks;
 using App.Api.Extensions;
 using App.Models.Domain.Identity;
 using App.Models.Domain.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using static QRCoder.PayloadGenerator;
+using static QRCoder.PayloadGenerator.OneTimePassword;
 
 namespace App.Api
 {
@@ -45,8 +52,99 @@ namespace App.Api
             {
                 UserName = currentUser.UserName,
                 Email = currentUser.Email,
-                GroupName = currentUser.Group?.Name
+                GroupName = currentUser.Group?.Name,
+
+                TwoFactorAuthentication = new {
+                    IsEnabled = await this._appUserManager.GetTwoFactorEnabledAsync(currentUser),
+                    IsAuthenticatorAppEnabled = (await this._appUserManager.GetAuthenticatorKeyAsync(currentUser)) != null
+                },
             });
+        }
+
+        [HttpPost("two-factor-authentication/pre-enable")]
+        public async Task<IActionResult> PreEnable() {
+            AppUser currentUser = await this._appUserManager.FindByIdAsync(this.User.Identity.GetUserId());
+            string key = await this.GetTwoFactorKeyAsync(currentUser);
+
+            return this.Ok(new {
+                QrCode = CreateBase64QRCode(key, currentUser),
+                SecretKey = FormatKey(key)
+            });
+        }
+
+        [HttpPost("two-factor-authentication")]
+        public async Task<IActionResult> Enable([FromBody] TwoFactorAuthenticationEnableInfo input) {
+            AppUser currentUser = await this._appUserManager.FindByIdAsync(this.User.Identity.GetUserId());
+            
+            string token = input.VerificationCode?.Replace("-", "").Replace(" ", "");
+            if (String.IsNullOrEmpty(token)) {
+                this.ModelState.AddModelError(nameof(input.VerificationCode), "Missing verification code");
+                return this.BadRequest(this.ModelState);
+            }
+            
+            bool isVerified = await this._appUserManager.VerifyTwoFactorTokenAsync(currentUser, this._appUserManager.Options.Tokens.AuthenticatorTokenProvider, token);
+            if (!isVerified) {
+                this.ModelState.AddModelError(nameof(input.VerificationCode), "Invalid verification code");
+                return this.BadRequest(this.ModelState);
+            }
+
+            await this._appUserManager.SetTwoFactorEnabledAsync(currentUser, true);
+            string[] recoveryCodes = (await this._appUserManager.GenerateNewTwoFactorRecoveryCodesAsync(currentUser, 10)).ToArray();
+
+            return this.Ok(new {
+                RecoveryCodes = recoveryCodes
+            });
+        }
+
+        [HttpDelete("two-factor-authentication")]
+        public async Task<IActionResult> Disable(){
+            AppUser currentUser = await this._appUserManager.FindByIdAsync(this.User.Identity.GetUserId());
+            await this._appUserManager.SetTwoFactorEnabledAsync(currentUser, false);
+            return this.NoContent();
+        }
+
+        private static string CreateBase64QRCode(string key, AppUser user) {
+            OneTimePassword qrPayload = new OneTimePassword {
+                Issuer = "Financial App",
+                Secret = key,
+                Type = OneTimePasswordAuthType.TOTP,
+                Label = user.UserName
+            };
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrPayload.ToString(), QRCodeGenerator.ECCLevel.L);
+
+            Base64QRCode qrCode = new Base64QRCode(qrCodeData);
+
+            return qrCode.GetGraphic(10);
+        }
+
+        private async Task<string> GetTwoFactorKeyAsync(AppUser user) {
+            await this._appUserManager.ResetAuthenticatorKeyAsync(user);
+            return await this._appUserManager.GetAuthenticatorKeyAsync(user);
+        }
+
+        private string FormatKey(string unformattedKey)
+        {
+            var result = new StringBuilder();
+            int currentPosition = 0;
+            while (currentPosition + 4 < unformattedKey.Length)
+            {
+                result.Append(unformattedKey.Substring(currentPosition, 4)).Append(" ");
+                currentPosition += 4;
+            }
+            if (currentPosition < unformattedKey.Length)
+            {
+                result.Append(unformattedKey.Substring(currentPosition));
+            }
+
+            return result.ToString().ToLowerInvariant();
+        }
+
+        [DataContract]
+        public class TwoFactorAuthenticationEnableInfo {
+            [DataMember(Name="verificationCode")]
+            public string VerificationCode { get;set; }
         }
     }
 }
--- src/App/js/AppFramework/ServerApi/Account.ts
+++ src/App/js/AppFramework/ServerApi/Account.ts
@@ -4,6 +4,26 @@ export interface IAccountInfo {
     userName: string;
     email: string;
     groupName: string;
+
+    twoFactorAuthentication: IAccountTwoFactorInfo;
+}
+
+export interface IAccountTwoFactorInfo {
+    isEnabled: boolean;
+    isAuthenticatorAppEnabled: boolean;
+}
+
+export interface ITwoFactorPreEnableInfo {
+    qrCode: string;
+    secretKey: string;
+}
+
+export interface ITwoFactorEnableInput {
+    verificationCode: string;
+}
+
+export interface ITwoFactorRecoveryCodes {
+    recoveryCodes: string[];
 }
 
 export class Api extends ApiBase {
@@ -15,5 +35,17 @@ export class Api extends ApiBase {
 
     public getInfo() {
         return this.execGet<IAccountInfo>('my-info');
+    }
+
+    public preEnable() {
+        return this.execPost<ITwoFactorPreEnableInfo>('two-factor-authentication/pre-enable');
+    }
+
+    public enable(input: ITwoFactorEnableInput) {
+        return this.execPost<ITwoFactorRecoveryCodes>('two-factor-authentication', input);
+    }
+
+    public disable() {
+        return this.execDelete('two-factor-authentication');
     }
 }
