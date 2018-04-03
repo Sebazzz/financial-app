d2a87eff4d92ed0c311ce5359f9cdedd5cbebc6a
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2018-04-03 21:05:58 +0200

    Use cleave for nice formatting of TFA code

--- src/App/js/App/BindingHandlers/Cleave.ts
+++ src/App/js/App/BindingHandlers/Cleave.ts
@@ -3,6 +3,45 @@ import * as ko from 'knockout';
 import * as $ from 'jquery';
 import {App} from 'AppFramework/AppFactory';
 
+ko.bindingHandlers.cleave = {
+    init(element: HTMLInputElement, valueAccessor: () => any, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel?: any, bindingContext?: KnockoutBindingContext): void {
+        const app = bindingContext && bindingContext.$root;
+
+        if (!app || !(app instanceof App)) {
+            throw new Error('Unable to find application view model');
+        }
+
+        const cleaveOptions = allBindingsAccessor.get('cleaveOptions'),
+              cleave = new Cleave(element, cleaveOptions);
+
+        ko.computed(() => {
+            let rawValue = ko.unwrap(valueAccessor());
+
+            if (rawValue === null || typeof rawValue === 'undefined') {
+                rawValue = '';
+            }
+
+            // Trigger JQuery event if necesary
+            $(element).trigger('change');
+
+            cleave.setRawValue(rawValue);
+        }, this, { disposeWhenNodeIsRemoved: element });
+
+        function updateObservable() {
+            const obs = valueAccessor();
+            if (ko.isWriteableObservable(obs)) {
+                const rawValue = cleave.getFormattedValue();
+
+                obs(rawValue);
+            }
+        }
+
+        element.addEventListener('blur', updateObservable);
+        ko.utils.domNodeDisposal.addDisposeCallback(element, () => cleave.destroy());
+        ko.utils.domNodeDisposal.addDisposeCallback(element, () => element.removeEventListener('blur', updateObservable));
+    }
+};
+
 ko.bindingHandlers.cleaveNumber = ko.bindingHandlers.cleaveCurrency = {
     init(element: HTMLInputElement, valueAccessor: () => any, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel?: any, bindingContext?: KnockoutBindingContext): void {
         const app = bindingContext && bindingContext.$root;
