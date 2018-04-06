4bfd46fca2efd7aef645df0e678e4a02aa286430
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2018-04-06 22:37:48 +0200

    Put SCSS rules in common file to keep it DRY
    Depending on the mode, we will append the correct loaders.

--- src/App/webpack.config.dev.js
+++ src/App/webpack.config.dev.js
@@ -5,44 +5,5 @@ module.exports = {
     devtool: "inline-source-map",
     plugins: [
         new webpack.NamedModulesPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        // CSS is outputted in the JS file to support HMR
-                        loader: "style-loader",
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: true,
-                        },
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            plugins: function() {
-                                return [
-                                    require("autoprefixer"),
-                                ];
-                            },
-                            sourceMap: true,
-                        },
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            includePaths: [
-                                "./node_modules",
-                            ],
-                            sourceMap: true,
-                        },
-                    },
-                ],
-            },
-        ],
-    },
+    ]
 };
