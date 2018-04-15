b1e34b4869ea8d2250610d19fe4dd0f6e6f0cf75
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2018-04-15 11:20:40 +0200

    For #17, implement duplicate entering password to prevent mistakes

---
+++ src/App/Models/DTO/AppUserMutate.cs
@@ -0,0 +1,7 @@
+﻿namespace App.Models.DTO {
+    public class AppUserMutate : AppUserListing {
+        public string NewPassword { get; set; }
+
+        public string CurrentPassword { get; set; }
+    }
+}
---
+++ src/App/Models/DTO/ChangePasswordModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Models.DTO {
+    public class ChangePasswordModel : IValidatableObject {
+        [Required(ErrorMessage = "Voer een nieuw wachtwoord in")]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Voer een nieuw wachtwoord nogmaals in")]
+        public string NewPasswordConfirm { get; set; }
+
+        [Required(ErrorMessage = "Voer je huidige wachtwoord in")]
+        public string CurrentPassword { get; set; }
+
+        /// <inheritdoc />
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(this.NewPasswordConfirm) && this.NewPassword != this.NewPasswordConfirm)
+            {
+                yield return new ValidationResult("De wachtwoorden komen niet overeen", new []{ nameof(this.NewPasswordConfirm) });
+            }
+        }
+    }
+}
