ea7fde025751c1c9b5b64361083b78b51ba7fe7b
Sebazzz <triadsebas@gmail.com>
2018-04-22 17:30:17 +0200

    Implement e-mail notifications for important account events
    E-mails are now sent if:
    - Password changed
    - Two factor authentication enabled / disabled

---
+++ src/App/Support/Mailing/PasswordChangeNotificationMailer.cs
@@ -0,0 +1,29 @@
+﻿// ******************************************************************************
+//  © 2018 Sebastiaan Dammann | damsteen.nl
+// 
+//  File:           : PasswordChangeNotificationMailer.cs
+//  Project         : App
+// ******************************************************************************
+
+namespace App.Support.Mailing {
+    using System.Threading.Tasks;
+
+    public sealed class PasswordChangeNotificationMailer {
+        private const string TemplateName = "password-change-notification";
+
+        private readonly MailService _mailService;
+        private readonly TemplateProvider _templateProvider;
+
+        public PasswordChangeNotificationMailer(MailService mailService, TemplateProvider templateProvider) {
+            this._mailService = mailService;
+            this._templateProvider = templateProvider;
+        }
+
+        public async Task SendAsync(string to, string userName) {
+            Template template = await this._templateProvider.GetTemplateAsync(TemplateName);
+            template.AddReplacement("user-name", userName);
+
+            await this._mailService.SendAsync(to, template);
+        }
+    }
+}
