8d7b0234044dc21fb6fd326a5b16f3a33f69757f
Sebazzz <triadsebas@gmail.com>
2018-04-26 22:32:56 +0200

    Weekly-digest: Install and configure Hangfire for sending weekly digest

---
+++ src/App/Support/Diagnostics/DiagnosticsOptions.cs
@@ -0,0 +1,12 @@
+﻿// ******************************************************************************
+//  © 2018 Sebastiaan Dammann | damsteen.nl
+// 
+//  File:           : DiagnosticsOptions.cs
+//  Project         : App
+// ******************************************************************************
+
+namespace App.Support.Diagnostics {
+    public class DiagnosticsOptions {
+        public string[] AllowedIPAddresses { get; set; }
+    }
+}
