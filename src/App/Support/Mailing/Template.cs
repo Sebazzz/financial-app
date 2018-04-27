ff40b9e6a1bcd5df45599c425994f3eaef8cad28
Sebazzz <triadsebas@gmail.com>
2018-04-27 23:01:12 +0200

    Implement a very basic repeater function for usage in compiled e-mail templates

--- src/App/Support/Mailing/Template.cs
+++ src/App/Support/Mailing/Template.cs
@@ -7,8 +7,28 @@
 
 namespace App.Support.Mailing {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.InteropServices;
     using System.Text;
     using System.Text.RegularExpressions;
+
+    [StructLayout(LayoutKind.Auto)]
+    public struct TemplateReplacementList {
+        private readonly StringBuilder _contents;
+        private readonly int _offset;
+
+        public TemplateReplacementList(StringBuilder contents, int offset) {
+            this._offset = offset;
+            this._contents = contents;
+        }
+        public TemplateReplacementList Replace(string search, string replacement) {
+            string searchToken = '{' + search + '}';
+            this._contents.Replace(searchToken, replacement, this._offset, this._contents.Length - this._offset);
+
+            return this;
+        }
+    }
 
     public sealed class Template {
         private readonly StringBuilder _contents;
@@ -34,26 +54,78 @@ namespace App.Support.Mailing {
             };
         }
 
+        public void RepeatSection<T>(string sectionName, ICollection<T> data, Action<T, TemplateReplacementList> dataBindAction) {
+            if (data.Count == 0) {
+                this.RemoveSection(sectionName);
+                return;
+            }
+
+            (int StartIndex, int Length, int InnerStartIndex, int InnerLength)? section = this.FindSection(sectionName);
+
+            if (section == null) {
+                return;
+            }
+
+            (int startIndex, int length, int innerStartIndex, int innerLength) = section.Value;
+
+            // We know how many items we got, so iterate reverse and append save one
+            // ... Strip end comment
+            int endCommentLength = length - innerLength - (innerStartIndex - startIndex);
+            this._contents.Remove(innerStartIndex + innerLength, endCommentLength);
+
+            // ... Prepare repeated template insertion
+            int insertOffset = innerStartIndex + innerLength;
+            char[] templateSource = new char[innerLength];
+            this._contents.CopyTo(innerStartIndex, templateSource, 0, templateSource.Length);
+
+            foreach (T dataItem in data.Skip(1).Reverse()) {
+                this._contents.Insert(insertOffset, templateSource);
+                dataBindAction.Invoke(dataItem, new TemplateReplacementList(this._contents, insertOffset));
+            }
+
+            // ... Replace final string - first item
+            {
+                T dataItem = data.First();
+                dataBindAction.Invoke(dataItem, new TemplateReplacementList(this._contents, innerStartIndex));
+
+                this._contents.Remove(startIndex, innerStartIndex - startIndex);
+            }
+        }
+
         public override string ToString() {
             return this._contents.ToString();
         }
 
-        public void HideSection(string sectionTitle) {
-            string commentStart = $"<!-- {sectionTitle} -->";
-            string commentEnd = $"<!-- /{sectionTitle} -->";
+        public void RemoveSection(string sectionName) {
+            var section = this.FindSection(sectionName);
 
-            int startIndex = this._contents.IndexOf(commentStart);
-            if (startIndex == -1) {
+            if (section == null) {
                 return;
             }
 
-            int endIndex = this._contents.IndexOf(commentEnd, startIndex);
-            if (endIndex == -1) {
-                return;
-            }
-
-            int length = endIndex - startIndex + commentEnd.Length;
+            (int startIndex, int length, _, _) = section.Value;
             this._contents.Remove(startIndex, length);
+        }
+
+        private (int StartIndex, int Length, int InnerStartIndex, int InnerLength)? FindSection(string sectionName) {
+            string commentStart = $"<!-- {sectionName} -->";
+            string commentEnd = $"<!-- /{sectionName} -->";
+
+            int outerStartIndex = this._contents.IndexOf(commentStart);
+            if (outerStartIndex == -1) {
+                return null;
+            }
+
+            int innerEndIndex = this._contents.IndexOf(commentEnd, outerStartIndex);
+            if (innerEndIndex == -1) {
+                return null;
+            }
+
+            int innerStartIndex = commentStart.Length + outerStartIndex;
+            int outerLength = commentEnd.Length + innerEndIndex - outerStartIndex;
+            int innerLength = outerLength - commentStart.Length - commentEnd.Length;
+
+            return (outerStartIndex, outerLength, innerStartIndex, innerLength);
         }
     }
 
