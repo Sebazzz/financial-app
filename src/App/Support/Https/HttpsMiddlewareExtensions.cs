29caa250896e4b9f98a5839bc5f3fc4a99e96684
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2018-09-16 17:57:07 +0200

    Enable built-in middleware for HTTPS redirection

--- src/App/Support/Https/HttpsMiddlewareExtensions.cs
+++ src/App/Support/Https/HttpsMiddlewareExtensions.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace App.Support.Https
 {
@@ -6,6 +8,13 @@ namespace App.Support.Https
     {
         public static IApplicationBuilder UseHttps(this IApplicationBuilder app)
         {
+            var httpsOptions = app.ApplicationServices.GetService<IOptions<HttpsServerOptions>>();
+
+            if (httpsOptions?.Value?.EnableRedirect == true)
+            {
+                app.UseHttpsRedirection();
+            }
+
             return app.UseMiddleware<HttpsMiddleware>();
         }
     }
