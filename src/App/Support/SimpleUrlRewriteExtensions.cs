43404815b4230f6b34414827774f1d41ba29c665
Sebazzz <triadsebas@gmail.com>
2018-09-26 22:10:11 +0200

    Rewrite serviceworker target correctly

---
+++ src/App/Support/SimpleUrlRewriteExtensions.cs
@@ -0,0 +1,22 @@
+﻿// ******************************************************************************
+//  © 2018 Sebastiaan Dammann | damsteen.nl
+// 
+//  File:           : SimpleUrlRewriteExtensions.cs
+//  Project         : App
+// ******************************************************************************
+
+namespace App.Support {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
+
+    public static class SimpleUrlRewriteExtensions {
+        public static void UseSimpleUrlRemap(this IApplicationBuilder app, PathString incoming, PathString rewritten) {
+            app.Use((ctx, next) => {
+                if (ctx.Request.Path.StartsWithSegments(new PathString(incoming))) {
+                    ctx.Request.Path = new PathString(rewritten);
+                }
+                return next();
+            });
+        }
+    }
+}
