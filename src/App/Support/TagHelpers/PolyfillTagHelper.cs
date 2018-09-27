8cd6ff53345acdfe3f89422b7470f53aac6195c0
Sebazzz <triadsebas@gmail.com>
2018-09-27 20:12:05 +0200

    Dynamically load stylesheet in production setting. Best workaround we have at the moment.

--- src/App/Support/TagHelpers/PolyfillTagHelper.cs
+++ src/App/Support/TagHelpers/PolyfillTagHelper.cs
@@ -8,10 +8,7 @@
 using System;
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Hosting.Internal;
-using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.Mvc.TagHelpers.Internal;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
@@ -21,14 +18,14 @@ using Microsoft.Extensions.Caching.Memory;
 namespace App.Support.TagHelpers
 {
     [HtmlTargetElement("script", Attributes = PolyfillTestAttribute)]
-    public class PolyfillTagHelper : TagHelper {
+    public class ScriptPolyfillTagHelper : TagHelper {
         private const string PolyfillTestAttribute = "fa-polyfill-test";
         private const string HtmlSrcAttribute = "src";
 
         private FileVersionProvider _fileVersionProvider;
         private readonly IMemoryCache _cache;
         private readonly IHostingEnvironment _hostingEnvironment;
-        public PolyfillTagHelper(IHostingEnvironment hostingEnvironment, IMemoryCache cache)
+        public ScriptPolyfillTagHelper(IHostingEnvironment hostingEnvironment, IMemoryCache cache)
         {
             this._hostingEnvironment = hostingEnvironment;
             this._cache = cache;
@@ -59,7 +56,7 @@ namespace App.Support.TagHelpers
             // Retrieve the TagHelperOutput variation of the "src" attribute in case other TagHelpers in the
             // pipeline have touched the value. If the value is already encoded this ScriptTagHelper may
             // not function properly.
-            TagHelperAttribute srcAttribute = output.Attributes["src"];
+            TagHelperAttribute srcAttribute = output.Attributes[HtmlSrcAttribute];
             this.Src = srcAttribute?.Value as string;
 
             if (srcAttribute == null)
@@ -83,6 +80,76 @@ namespace App.Support.TagHelpers
                 this._fileVersionProvider = new FileVersionProvider(
                     this._hostingEnvironment.WebRootFileProvider,
                     this._cache, 
+                    this.ViewContext.HttpContext.Request.PathBase);
+            }
+        }
+
+        // We need this tag helper to execute as last
+        public override int Order => 1000;
+    }
+
+    [HtmlTargetElement("link", Attributes = PolyfillTestAttribute)]
+    public class LinkPolyfillTagHelper : TagHelper {
+        private const string PolyfillTestAttribute = "fa-polyfill-test";
+        private const string HtmlHrefAttribute = "href";
+
+        private FileVersionProvider _fileVersionProvider;
+        private readonly IMemoryCache _cache;
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public LinkPolyfillTagHelper(IHostingEnvironment hostingEnvironment, IMemoryCache cache) {
+            this._hostingEnvironment = hostingEnvironment;
+            this._cache = cache;
+        }
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        [HtmlAttributeName(PolyfillTestAttribute)]
+        public string PolyfillTest { get; set; }
+
+        [HtmlAttributeName(HtmlHrefAttribute)]
+        public string Href { get; set; }
+
+        /// <inheritdoc />
+        public override void Process([App.NotNull] TagHelperContext context, [App.NotNull] TagHelperOutput output) {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+
+            // Pass through attribute that is also a well-known HTML attribute
+            if (this.Href != null) {
+                output.CopyHtmlAttribute(HtmlHrefAttribute, context);
+            }
+
+            // Retrieve the TagHelperOutput variation of the "href" attribute in case other TagHelpers in the
+            // pipeline have touched the value. If the value is already encoded this TagHelper may
+            // not function properly.
+            TagHelperAttribute hrefAttribute = output.Attributes[HtmlHrefAttribute];
+            this.Href = hrefAttribute?.Value as string;
+
+            if (hrefAttribute == null) {
+                return;
+            }
+
+            // Output path in polyfill detection code with version string
+            this.EnsureFileVersionProvider();
+
+            output.Attributes.Remove(hrefAttribute);
+            output.Attributes.RemoveAll("rel");
+
+            string path = this._fileVersionProvider.AddFileVersionToPath(this.Href);
+            output.TagName = "script";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Content.SetHtmlContent(
+                $"{this.PolyfillTest}||ss('{JavaScriptEncoder.Default.Encode(path)}')"
+            );
+        }
+
+        private void EnsureFileVersionProvider() {
+            if (this._fileVersionProvider == null) {
+                this._fileVersionProvider = new FileVersionProvider(
+                    this._hostingEnvironment.WebRootFileProvider,
+                    this._cache,
                     this.ViewContext.HttpContext.Request.PathBase);
             }
         }
