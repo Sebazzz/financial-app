85b8989d7790f28d4adf9536e3619955bc180992
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2018-09-30 17:12:54 +0200

    Add responsive rendering binding handler: only render and bind if media query is triggered

--- src/App/js/AppFramework/Client/BrowserDetector.ts
+++ src/App/js/AppFramework/Client/BrowserDetector.ts
@@ -25,6 +25,28 @@ function isMobileInternal(): boolean {
     return false;
 }
 
+function getMobileQueryMatch() {
+    isMobileInternal();
+
+    if (!mobileQueryMatch) {
+        throw new Error('mobileQueryMatch is null');
+    }
+
+    return mobileQueryMatch;
+}
+
+export function addResponsiveListener(listener: MediaQueryListListener) {
+    const matcher = getMobileQueryMatch();
+
+    matcher.addListener(listener);
+}
+
+export function removeResponsiveListener(listener: MediaQueryListListener) {
+    const matcher = getMobileQueryMatch();
+
+    matcher.addListener(listener);
+}
+
 export default function detectMobile() {
     return isMobileInternal();
 }
---
+++ src/App/js/AppFramework/Client/ResponsiveRenderBindingHandler.ts
@@ -0,0 +1,36 @@
+import * as ko from 'knockout';
+import { default as isMobile, addResponsiveListener, removeResponsiveListener } from './BrowserDetector';
+
+const enum ResponsiveRenderMode {
+    Desktop = 'desktop',
+    Mobile = 'mobile'
+}
+
+ko.bindingHandlers.responsiveRender = {
+    init(
+        element: Node,
+        valueAccessor: () => ResponsiveRenderMode,
+        allBindingsAccessor: KnockoutAllBindingsAccessor,
+        viewModel: any,
+        bindingContext: KnockoutBindingContext
+    ) {
+        const mode = valueAccessor(),
+            shouldRender = ko.observable(checkShouldRender(isMobile()));
+
+        function checkShouldRender(isMatch: boolean) {
+            return (mode === ResponsiveRenderMode.Mobile) === isMatch;
+        }
+
+        function updateShouldRender(mql: MediaQueryList) {
+            shouldRender(checkShouldRender(mql.matches));
+        }
+
+        // Act on media query change
+        addResponsiveListener(updateShouldRender);
+        ko.utils.domNodeDisposal.addDisposeCallback(element, () => removeResponsiveListener(updateShouldRender));
+
+        // Let the 'if' binding do the heavy lifting
+        return ko.bindingHandlers.if.init!(element, () => shouldRender, allBindingsAccessor, viewModel, bindingContext);
+    }
+};
+ko.virtualElements.allowedBindings.responsiveRender = true;
