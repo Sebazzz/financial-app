3a85ad54090820b2dad574e5ff4b12931c5d6983
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2018-09-30 18:32:31 +0200

    Nice "swipe transition to end" animation

--- src/App/js/AppFramework/BindingHandlers/SwipeActions.ts
+++ src/App/js/AppFramework/BindingHandlers/SwipeActions.ts
@@ -3,8 +3,13 @@ import 'swipe-listener';
 
 ko.bindingHandlers.swipeActions = {
     init(element: HTMLElement) {
-        const bodySelector = '.swipeable__body',
-            swipeableElement = element.querySelector(bodySelector) as HTMLElement | null;
+        const logPrefix = 'SwipeActions: ',
+            bodySelector = '.swipeable__body',
+            primaryActionSelector = '.swipeable__primary-action',
+            bodyAnimClass = bodySelector.substr(1) + '--is-settling',
+            swipeableElement = element.querySelector(bodySelector) as HTMLElement | null,
+            primaryActionElement =
+                swipeableElement && (swipeableElement.querySelector(primaryActionSelector) as HTMLElement | null);
         if (!swipeableElement || swipeableElement === null) {
             console.error(element);
             throw new Error('Error: Unable to find ' + bodySelector);
@@ -23,7 +28,8 @@ ko.bindingHandlers.swipeActions = {
             rightActions = element.querySelectorAll('.swipeable__action-right'),
             rightActionSize = countMaxSize(rightActions);
 
-        let currentOffset = 0;
+        let currentOffset = 0,
+            preventImmediateClick = false;
 
         const listener = SwipeListener(element, {
             minVertical: 1000 // Prevent top/bottom swipe
@@ -43,7 +49,7 @@ ko.bindingHandlers.swipeActions = {
         function onSwipeEvent(ev: SwipeEvent<IBasicSwipeDetails>) {
             const detail = ev.detail;
 
-            console.log('Swipe release: ' + JSON.stringify(detail));
+            console.log('[%s]: Swipe release: %', logPrefix, JSON.stringify(detail));
 
             const [xStart, xEnd] = detail.x,
                 xDiff = calculateDiff(xStart, xEnd),
@@ -51,12 +57,32 @@ ko.bindingHandlers.swipeActions = {
                 fullOpenSize = xDiff < 0 ? -1 * rightActionSize : leftActionSize,
                 cancelThreshold = directionMax / 2;
 
+            // Set-up animation
+            function onSwipeSettled() {
+                swipeableElement!.removeEventListener('transitionend', onSwipeSettled);
+                swipeableElement!.removeEventListener('transitioncancel', onSwipeSettled);
+
+                swipeableElement!.classList.remove(bodyAnimClass);
+            }
+
+            swipeableElement!.addEventListener('transitionend', onSwipeSettled);
+            swipeableElement!.addEventListener('transitioncancel', onSwipeSettled);
+            swipeableElement!.classList.add(bodyAnimClass);
+
+            // Force layout, otherwise transition won't happen (note: if this doesn't work out on Webkit, we can try .focus())
+            swipeableElement!.offsetHeight.toString();
+
             if (Math.abs(xDiff) < cancelThreshold) {
                 setSwipeState(0);
                 currentOffset = 0;
             } else {
                 setSwipeState(fullOpenSize);
                 currentOffset = xDiff;
+            }
+
+            if (xDiff > 5) {
+                preventImmediateClick = true;
+                window.setTimeout(() => (preventImmediateClick = false), 1);
             }
         }
 
@@ -66,16 +92,42 @@ ko.bindingHandlers.swipeActions = {
             const [xStart, xEnd] = detail.x,
                 xDiff = calculateDiff(xStart, xEnd);
 
-            console.info('Swiping %s-%s=%s', xStart, xEnd, xDiff);
+            console.info('[%s] Swiping %s-%s=%s', logPrefix, xStart, xEnd, xDiff);
             setSwipeState(xDiff);
         }
 
+        function onSwipeableElementClick(ev: MouseEvent) {
+            if (
+                !ev.target ||
+                !primaryActionElement ||
+                ev.target === primaryActionElement ||
+                primaryActionElement!.contains(ev.target as Node)
+            ) {
+                // Let it propagate
+                return;
+            }
+
+            // Trigger click on primary element
+            ev.preventDefault();
+            ev.stopPropagation();
+
+            if (preventImmediateClick) {
+                return;
+            }
+
+            console.log('[%s] Triggering primary action click', logPrefix);
+
+            primaryActionElement.click();
+        }
+
+        swipeableElement!.addEventListener('click', onSwipeableElementClick);
         element.addEventListener('swiperelease', onSwipeEvent);
         element.addEventListener('swiping', onSwipingEvent);
 
         ko.utils.domNodeDisposal.addDisposeCallback(element, () => {
             listener.off();
 
+            swipeableElement!.removeEventListener('click', onSwipeableElementClick);
             element.removeEventListener('swipe', onSwipeEvent);
             element.removeEventListener('swiping', onSwipingEvent);
         });
