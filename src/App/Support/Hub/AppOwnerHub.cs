f727a6c4e64e0cd1067dd3d73db0f7c189f581cf
Sebazzz <triadsebas@gmail.com>
2018-10-06 15:40:50 +0200

    Fixes SignalR for #29

--- src/App/Support/Hub/AppOwnerHub.cs
+++ src/App/Support/Hub/AppOwnerHub.cs
@@ -10,9 +10,9 @@
 
     public sealed class AppOwnerHub : Hub
     {
-        private readonly ILogger _logger;
+        private static readonly GroupContext<HashSet<string>> GroupContext = new GroupContext<HashSet<string>>(_ => new HashSet<string>(), s => new HashSet<string>(s));
 
-        private readonly GroupContext<HashSet<string>> _groupContext = new GroupContext<HashSet<string>>(_ => new HashSet<string>(), s => new HashSet<string>(s));
+        private readonly ILogger _logger;
 
         private string GetGroupName()
         {
@@ -29,7 +29,7 @@
             await this.Clients.Group(group).SendAsync("pushClient", this.Context.User.Identity.Name);
             await this.Groups.AddToGroupAsync(this.Context.ConnectionId, group);
 
-            string[] groupInfo = this._groupContext.AlterAndReturn(group, s => s.Add(identity)).ToArray();
+            string[] groupInfo = GroupContext.AlterAndReturn(group, s => s.Add(identity)).ToArray();
             await this.Clients.Client(this.Context.ConnectionId).SendAsync("setInitialClientList", new object[] { groupInfo });
 
             await base.OnConnectedAsync();
@@ -40,10 +40,12 @@
         /// </summary>
         public override async Task OnDisconnectedAsync(Exception exception)
         {
-            this._logger.LogInformation("Connection disconnected, user: {0}", this.Context.User.Identity.Name);
-
+            string identity = this.Context.User.Identity.Name;
             string group = this.GetGroupName();
 
+            this._logger.LogInformation("Disconnecting, user: {0} of group {1}", identity, group);
+
+            GroupContext.AlterAndReturn(group, s => s.Remove(group));
             await this.Clients.Group(group).SendAsync("popClient", this.Context.User.Identity.Name);
 
             await base.OnDisconnectedAsync(exception);
