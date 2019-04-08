3804ccf8eb546205dbb08466ecd89b106a916692
Sebazzz <triadsebas@gmail.com>
2019-04-08 22:35:01 +0200

    Knockout 3.5.0 upgrade
    Not having paid attention

--- src/App/js/AppFramework/BindingHandlers/LookupScope.ts
+++ src/App/js/AppFramework/BindingHandlers/LookupScope.ts
@@ -3,7 +3,7 @@ import * as ko from 'knockout';
 type PropertySelector<TSource, TIdentifier> = (item: TSource) => TIdentifier;
 
 interface ISubstituteScopeOptions<TSource, TIdentifier> {
-    data: TIdentifier[] | ko.ObservableArray<TIdentifier> | TIdentifier | ko.Observable<TIdentifier>;
+    data: ko.MaybeObservableArray<TIdentifier> | ko.MaybeObservable<TIdentifier>;
     source: TSource[] | ko.ObservableArray<TSource>;
 
     selector: string | PropertySelector<TSource, TIdentifier>;
@@ -26,48 +26,45 @@ ko.bindingHandlers.lookupScope = {
                 typeof options.selector === 'string'
                     ? makeAccessor<TSource, TIdentifier>(options.selector)
                     : options.selector,
-            childData = ko.computed(
-                {
-                    read: () => {
-                        const identifiers = ko.unwrap<TIdentifier[] | TIdentifier>(options.data),
-                            sourceData = ko.unwrap(options.source);
+            childData = ko.computed<TSource | TSource[]>({
+                read: () => {
+                    const identifiers = ko.unwrap<TIdentifier[] | TIdentifier>(options.data);
+                    const sourceData = ko.unwrap(options.source);
 
-                        if (sourceData === null) {
-                            throw new Error('lookupScope: options.source is null');
+                    if (sourceData === null) {
+                        throw new Error('lookupScope: options.source is null');
+                    }
+
+                    if (!Array.isArray(identifiers)) {
+                        const identifier = identifiers,
+                            lookup = sourceData.filter(val => identifier === accessor(val))[0];
+
+                        return lookup;
+                    }
+
+                    return sourceData.filter(val => identifiers.indexOf(accessor(val)) !== -1);
+                },
+                write: (data: TSource[] | TSource) => {
+                    if (!ko.isObservable<TIdentifier | TIdentifier[]>(options.data)) {
+                        return;
+                    }
+
+                    if ($.isArray(ko.utils.peekObservable<TIdentifier[] | TIdentifier>(options.data))) {
+                        // case: target is array
+                        if (ko.isWriteableObservable<TIdentifier | TIdentifier[]>(options.data)) {
+                            (options.data as ko.ObservableArray<TIdentifier>)(
+                                (data as TSource[]).map(val => accessor(val))
+                            );
                         }
-
-                        if (!$.isArray(identifiers)) {
-                            const identifier = identifiers,
-                                lookup = sourceData.filter(val => identifier === accessor(val))[0];
-
-                            return lookup;
-                        }
-
-                        return sourceData.filter(val => identifiers.indexOf(accessor(val)) !== -1);
-                    },
-                    write: (data: TSource[] | TSource) => {
-                        if (!ko.isObservable<TIdentifier | TIdentifier[]>(options.data)) {
-                            return;
-                        }
-
-                        if ($.isArray(ko.utils.peekObservable<TIdentifier[] | TIdentifier>(options.data))) {
-                            // case: target is array
-                            if (ko.isWriteableObservable<TIdentifier | TIdentifier[]>(options.data)) {
-                                (options.data as ko.ObservableArray<TIdentifier>)(
-                                    (data as TSource[]).map(val => accessor(val))
-                                );
-                            }
-                        } else {
-                            // case: target is single-value
-                            if (ko.isWriteableObservable<TIdentifier | TIdentifier[]>(options.data)) {
-                                (options.data as ko.Observable<TIdentifier>)(accessor(data as TSource));
-                            }
+                    } else {
+                        // case: target is single-value
+                        if (ko.isWriteableObservable<TIdentifier | TIdentifier[]>(options.data)) {
+                            (options.data as ko.Observable<TIdentifier>)(accessor(data as TSource));
                         }
                     }
                 },
-                null,
-                { disposeWhenNodeIsRemoved: element as any /* knockout/issues/2471 */ }
-            );
+                disposeWhenNodeIsRemoved: element as any /* knockout/issues/2471 */
+            });
 
         const ctx = bindingContext.extend({
             $lookup: childData
