9889dfba1de019bdedfe72bf9d2053b635821add
Sebazzz <triadsebas@gmail.com>
2019-06-02 12:15:21 +0200

    Fix format invocation

--- pre-commit.ps1
+++ pre-commit.ps1
@@ -1,9 +1,11 @@
 # Check if dotnet-format is installed
+$ENV:PATH = $ENV:PATH + [System.IO.Path]::PathSeparator + $(Join-Path $PSScriptRoot ".dotnet")
+
 if (!(Get-Command "dotnet-format" -ErrorAction SilentlyContinue)) {
     Write-Host "dotnet-format not installed - installing"
     New-Item ".dotnet" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 
-    dotnet.exe tool install --tool-path .dotnet/ dotnet-format
+    dotnet tool install --tool-path .dotnet/dotnet-format dotnet-format
 
     if ($LASTEXITCODE -ne 0) {
         return $LASTEXITCODE
